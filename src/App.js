155a18d5787f67fc0ef33a1353bd12b5f8c02b55
Prabodashan <prabodashan14@gmail.com>
2024-05-29 13:32:36 +0530

    Created regidter and login page with functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
+
   return (
     <div className="dark">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
+          <Route
+            exact
+            path="/"
+            element={token ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            exact
+            path="/login"
+            element={!token ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            exact
+            path="/register"
+            element={!token ? <Register /> : <Navigate to="/" />}
+          />
         </Routes>
       </Router>
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import LoginForm from "../components/section/auth/LoginForm";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
+      {/*Container*/}
+      <div className="flex w-[1600px] mx-auto h-full">
+        {/*Login Form */}
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
